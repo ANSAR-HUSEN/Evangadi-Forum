28c774f21940604684a71bf5af5025e9e8e8e136
Bitbitb <bitfirbet@gmail.com>
2024-10-02 19:22:49 -0400

    Completed footer and question pages. 	used icons from material icon, and extracted Evangadi loge for footer page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 
-import React from 'react'
+import React, {useState } from 'react'
+import Footer from './components/footer/Footer'
+import QuestionPage from './components/questionPage/QuestionPage';
+
 
 function App() {
+
+  const [answers, setAnswers] = useState([]);
+
+  const handlePostAnswer = (answer) => {
+    setAnswers((prevAnswers) => [...prevAnswers, answer]);
+  };
+
   return (
-    <div><h1>EVANGADI FORUM</h1></div>
-  )
+    <>
+      <div>
+        
+      </div>
+      <QuestionPage/>
+      <Footer />
+    </>
+  );
 }
 
 export default App

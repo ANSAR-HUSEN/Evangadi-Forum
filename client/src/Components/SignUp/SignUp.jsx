d690df72c432ba814f1b1d405556d0e55cbd19fb
Moltish <moltotalm@gmail.com>
2024-10-02 13:31:02 -0400

    Modified the SignUp jsx and css

--- client/src/Components/SignUp/SignUp.jsx
+++ client/src/Components/SignUp/SignUp.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./SignUp.css";
+import { FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+import { FaEye } from "react-icons/fa";
 
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div>
       <div className="signUp">
         <div className="accountTitle">
           <h1>Join the network</h1>
           <h3>
-            Already have an account? <a href="">Sign in</a>
+            Already have an account?{" "}
+            <a href="" target="_blank">
+              Sign in
+            </a>
           </h3>
         </div>
         <div>
@@ -17,12 +28,18 @@ function SignUp() {
               <input type="email" placeholder="Email" />
             </div>
             <div className="smallInput">
-              <input type="text" placeholder="First Name" />
+              <input className="second" type="text" placeholder="First Name" />
               <input type="text" placeholder="Last Name" />
             </div>
             <div className="bigInput">
               <input type="text" placeholder="User Name" />
-              <input type="text" placeholder="Password" />
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+              />
+              <button className="eye" onClick={togglePasswordVisibility}>
+                {showPassword ? <FaEye /> : <FaEyeSlash />}
+              </button>
             </div>
             <div className="btn">
               <button>Agree and Join</button>
@@ -30,11 +47,19 @@ function SignUp() {
           </form>
           <div>
             <h4>
-              I agree to the <a href="">privacy policy</a> and{" "}
-              <a href="">terms of service.</a>
+              I agree to the{" "}
+              <a href="" target="_blank">
+                privacy policy
+              </a>{" "}
+              and{" "}
+              <a href="" target="_blank">
+                terms of service.
+              </a>
             </h4>
             <h4>
-              <a href="">Already have an account?</a>
+              <a href="" target="_blank">
+                Already have an account?
+              </a>
             </h4>
           </div>
         </div>

65a883497f7020c3c2142ecb40edae48eca5b7df
wanditt <79541813+wanditt@users.noreply.github.com>
2024-10-09 15:11:45 -0400

    Deploying on Netlify

--- client/src/Components/QuestionDisplay.jsx
+++ client/src/Components/QuestionDisplay.jsx
@@ -1,19 +1,19 @@
 // src/components/QuestionDisplay.jsx
-import React from 'react';
+import React from "react";
 
 const QuestionDisplay = ({ question, answers }) => {
-    return (
-        <div className="question-display">
-            <h2>{question.title}</h2>
-            <p>{question.body}</p>
-            <h3>Answers:</h3>
-            <ul>
-                {answers.map((answer) => (
-                    <li key={answer.id}>{answer.body}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="question-display">
+      <h2>{question.title}</h2>
+      <p>{question.body}</p>
+      <h3>Answers:</h3>
+      <ul>
+        {answers.map((answer) => (
+          <li key={answer.id}>{answer.body}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default QuestionDisplay;
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,38 +1,51 @@
-const { StatusCodes} = require('http-status-codes')
-const jwt = require('jsonwebtoken')
-const dotenv= require ('dotenv')
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 dotenv.config();
-async function authMiddleware (req,res,next) {
+// async function authMiddleware (req,res,next) {
 
-    const authHeader = req.headers.authorization
-    //validate token exists 
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
-        return res
-           .status(StatusCodes.UNAUTHORIZED)
-           .json({ msg: "Authentication failed" });
-    }
-    const token = authHeader.split(' ')[1]
-    console.log(authHeader);
-    console.log(token);    
+//     const authHeader = req.headers.authorization
+//     //validate token exists
+//     if(!authHeader || !authHeader.startsWith('Bearer')) {
+//         return res
+//            .status(StatusCodes.UNAUTHORIZED)
+//            .json({ msg: "Authentication failed" });
+//     }
+//     const token = authHeader.split(' ')[1]
+//     console.log(authHeader);
+//     console.log(token);
 
-    try {
-        const { username, userid } = jwt.verify(token, process.env.JWT_SECRET);
-       
-       // return res.status(StatusCodes.OK).json({data})//valid token passed 
-       req.user = { username, userid } //req.user is a custom property and will pass to next()
+//     try {
+//         const { username, userid } = jwt.verify(token, process.env.JWT_SECRET);
 
-       next() //next is called once authentication is successfull 
+//        // return res.status(StatusCodes.OK).json({data})//valid token passed
+//        req.user = { username, userid } //req.user is a custom property and will pass to next()
 
+//        next() //next is called once authentication is successfull
 
+//     } catch (error) {
+//         return res
+//            .status(StatusCodes.UNAUTHORIZED)
+//            .json({ msg: "Authentication failed" });
+//     }
 
+// }
 
-    } catch (error) {
-        return res
-           .status(StatusCodes.UNAUTHORIZED)
-           .json({ msg: "Authentication failed" });
-    }
-
-
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization;
+  if (!token || !token.startsWith("Bearer")) {
+    return res.status(401).json({
+      error: "Unauthorized",
+      message: "Authentication invalid",
+    });
+      
+      const tokenString = token.split(' ')[1]
+      
+      try {
+        jwt.verify(tokenString,)
+      } catch (error) {
+        
+      }
+  }
 }
-
-module.exports = authMiddleware
+module.exports = authMiddleware;

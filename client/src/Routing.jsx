d47ba424dba97b9d1b62bc02d653097694750f2b
birukjira <birukjira@gmail.com>
2024-10-10 05:19:39 +0300

    Revert "Implement Sign-up and Sign-in Page"
    This reverts commit a1d103d7acd169dac17980b606de1edbac92114e.

--- client/src/Routing.jsx
+++ client/src/Routing.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home'
 // import Login from './Pages/Login/Login'
-// import QuestionPage from "./Components/questionPage/QuestionPage";
-// import SignUp from "./Components/SignUp/SignUp";
-// import About from "./Components/About/About";
-// import QuestionDisplay from "./Components/QuestionDisplay";
-import Auth from "./Pages/Auth/Auth";
+import QuestionPage from './Components/questionPage/QuestionPage';
+import SignUp from './Components/SignUp/SignUp'
+import About from './Components/About/About';
+import QuestionDisplay from './Components/QuestionDisplay';
+
+
+
+
 
 function Routing() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Auth />} />
-      <Route path="/register" element={<Auth />} />
-      {/* <Route path="/questionPage" element={<QuestionPage />} /> */}
-      {/* <Route path="/questionDetail" element={<QuestionDisplay/>} /> */}
-    </Routes>
+     <div>
+        <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/login" element={<About />} />
+           <Route path="/register" element={<SignUp/>} />
+           <Route path="/questionPage" element={<QuestionPage/>} />
+           {/* <Route path="/questionDetail" element={<QuestionDisplay/>} /> */}
+        </Routes>
+     </div>
   );
 }
 
-export default Routing;
+export default Routing

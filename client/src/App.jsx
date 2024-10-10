eb694800e8121c573311ea23098e6debdb3bfaf9
ANSAR HUSEN <ansar.husen-ug@aau.edu.et>
2024-10-11 01:06:54 +0300

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+
+
+import React, {useState } from 'react'
+import Footer from './components/footer/Footer'
+
+
+
+import Home from "./Pages/Home/Home";
+
+
+import SignUp from "./components/SignUp/SignUp";
+
+
+
+import Header from './componet/header/header'
+
+import Routing from './Routing'
+
+import { Router } from 'react-router-dom'
+
+
+
+import Routing from "./Routing";
+
+function App() {
+  const [answers, setAnswers] = useState([]);
+
+  const handlePostAnswer = (answer) => {
+    setAnswers((prevAnswers) => [...prevAnswers, answer]);
+  };
+
+  return (
+    <>
+      <Routing />
+    </>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const PORT = process.env.PORT || 5501;
+
+// const cors = require("cors");
+app.use(cors());
+
+// Connect to database
+const dbConnection = require('./db/dbConfig');
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+
+// User Routes middleware file
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
+// Question Routes middleware file
+const questionsRoutes = require("./routes/questionRoutes");
+app.use("/api", questionsRoutes);
+
+// Answer Routes middleware file
+const answerRoutes = require('./routes/answerRoutes');
+app.use("/api", answerRoutes);
+
+// const userRoutes = require("./routes/userRoutes");
+
+// Question Routes middleware file
+const questionRoutes = require("./routes/questionRoutes");
+
+// Answer Routes middleware file
+// const answerRoutes = require("./routes/answerRoutes");
+
+// user rotes middleware
+app.use('/api/users', userRoutes);
+
+// Use question routes
+
+
+// Use answer routes
+app.use('/api', answerRoutes)
+
+
+// Start server
+async function start() {
+  try {
+
+    // Checking database connection
+    await dbConnection.execute("SELECT 1"); // Simple query to ensure DB connection
+    console.log("Database connection established");
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+    process.exit(1); // Exit the process if DB connection fails
+  }
+}
+
+// Start the server
+start()

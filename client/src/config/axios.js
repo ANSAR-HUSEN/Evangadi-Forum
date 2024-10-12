b6ae0ed035e3ad6ff0fe7995ff6b1090923702e6
Moltish <moltotalm@gmail.com>
2024-10-11 21:22:16 -0400

    initial commit

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
-import axios from "axios"
+import axios from "axios";
 
 const axiosBase = axios.create({
-    baseURL:"http://localhost:5500/api"
-})
+  baseURL: "https://evanapi.birukjira.com/api",
+});
 
 export default axiosBase;
